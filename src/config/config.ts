111125b44750f56c66c0fd86a02b4632af815060
Gordiart <sattriel@gmail.com>
2023-03-25 11:55:17 -0400

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,12 @@ const unlockables: Unlockable[] = [
       "0xfbfe83e8bbf85e787cd0f38b17804e0fe577f73911d55832bc33396cf348f5ca",
     ],
   },
+  {
+    cid: "bafybeiab3dn7fjd7haffjp36grauoseg2qtkicxglkpi4u6xtkunpemdpi",
+    nftId: [
+      "0x77f458f230112c2645facd0b9efda186212ee92d92a06226a409c0309761a74a",
+    ],
+  },
 ];
 
 export { unlockables };
