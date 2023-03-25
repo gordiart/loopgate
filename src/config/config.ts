32ffff8c92827ee929bd58bf2b50789345ab04b4
Gordiart <sattriel@gmail.com>
2023-03-25 11:38:48 -0400

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ const unlockables: Unlockable[] = [
   {
     cid: "bafybeicsknf7wnlrumaipcaf4j4mu66ypj3buxx63hiqh4yce5ts5whpye",
     nftId: [
-      "0x45cde8f2973214adbb3c2d7eaa78f2bfe7eefac7f3400aa28f6bba0ee3e8f6fa",
+      "0xfbfe83e8bbf85e787cd0f38b17804e0fe577f73911d55832bc33396cf348f5ca",
     ],
   },
 ];
