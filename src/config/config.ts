1711681e8ed113e3a313a066663d0623c83fd509
Gordiart <sattriel@gmail.com>
2023-03-24 21:44:25 -0400

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -23,20 +23,20 @@ const unlockables: Unlockable[] = [
   {
     cid: "bafybeicsknf7wnlrumaipcaf4j4mu66ypj3buxx63hiqh4yce5ts5whpye", // HTML blog example
     nftId: [
-      "0x45cde8f2973214adbb3c2d7eaa78f2bfe7eefac7f3400aa28f6bba0ee3e8f6fa",
+      "0x621987efbf4d432f42b39ca0320d1ed63e239680a11419e6dd228e68f35e0291",
     ], // You need to own 'GM #1' to unlock this.
   },
   {
     cid: "bafybeicsknf7wnlrumaipcaf4j4mu66ypj3buxx63hiqh4yce5ts5whpye", // MP4 video example
     nftId: [
-      "0x45cde8f2973214adbb3c2d7eaa78f2bfe7eefac7f3400aa28f6bba0ee3e8f6fa",
+      "0x621987efbf4d432f42b39ca0320d1ed63e239680a11419e6dd228e68f35e0291",
     ], // You need to own 'GM #2' to unlock this.
   },
   {
     cid: "bafybeicsknf7wnlrumaipcaf4j4mu66ypj3buxx63hiqh4yce5ts5whpye", // Web game example
     nftId: [
-      "0x45cde8f2973214adbb3c2d7eaa78f2bfe7eefac7f3400aa28f6bba0ee3e8f6fa",
-      "0x45cde8f2973214adbb3c2d7eaa78f2bfe7eefac7f3400aa28f6bba0ee3e8f6fa",
+      "0x621987efbf4d432f42b39ca0320d1ed63e239680a11419e6dd228e68f35e0291",
+      "0x621987efbf4d432f42b39ca0320d1ed63e239680a11419e6dd228e68f35e0291",
     ], // You need to own 'GM #1' & 'GM #2' to unlock this.
   },
 ];
