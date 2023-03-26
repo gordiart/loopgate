e007413f5afccc0a9075f1025dd0cd36ff7920aa
Gordiart <sattriel@gmail.com>
2023-03-26 08:43:46 -0400

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,19 @@
 import { Unlockable } from "./types";
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeicsknf7wnlrumaipcaf4j4mu66ypj3buxx63hiqh4yce5ts5whpye",
+    cid: "bafybeiarfkqudaep2es6lusm6sw3dmiipanvqkxalwpon27ajszwkb3npy",
     nftId: [
       "0xfbfe83e8bbf85e787cd0f38b17804e0fe577f73911d55832bc33396cf348f5ca",
     ],
   },
   {
-    cid: "bafybeiab3dn7fjd7haffjp36grauoseg2qtkicxglkpi4u6xtkunpemdpi",
+    cid: "",
     nftId: [
       "0x77f458f230112c2645facd0b9efda186212ee92d92a06226a409c0309761a74a",
     ],
   },
   {
-    cid: "bafybeifkxchibgufjemx2aglnz7dmlcamynujywhiykd4hqex73jwcok4u",
+    cid: "",
     nftId: [
       "0x77f458f230112c2645facd0b9efda186212ee92d92a06226a409c0309761a74a",
     ],
