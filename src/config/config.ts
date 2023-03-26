5f309e22c00dde29eabe22d3f1981f321b98d745
Gordiart <sattriel@gmail.com>
2023-03-26 08:46:14 -0400

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,19 @@
 import { Unlockable } from "./types";
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiarfkqudaep2es6lusm6sw3dmiipanvqkxalwpon27ajszwkb3npy",
+    cid: "bafybeiarfkqudaep2es6lusm6sw3dmiipanvqkxalwpon27ajszwkb3npy", //RYUI BANG FILES
     nftId: [
       "0xfbfe83e8bbf85e787cd0f38b17804e0fe577f73911d55832bc33396cf348f5ca",
     ],
   },
   {
-    cid: "",
+    cid: "bafybeiarfkqudaep2es6lusm6sw3dmiipanvqkxalwpon27ajszwkb3npy",
     nftId: [
       "0x77f458f230112c2645facd0b9efda186212ee92d92a06226a409c0309761a74a",
     ],
   },
   {
-    cid: "",
+    cid: "bafybeiarfkqudaep2es6lusm6sw3dmiipanvqkxalwpon27ajszwkb3npy",
     nftId: [
       "0x77f458f230112c2645facd0b9efda186212ee92d92a06226a409c0309761a74a",
     ],
