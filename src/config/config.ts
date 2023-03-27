8206ab926ee72193ac77f2f7fc938aaeb139aaaa
Gordiart <sattriel@gmail.com>
2023-03-27 19:09:59 -0400

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,24 @@ const unlockables: Unlockable[] = [
       "0x45cde8f2973214adbb3c2d7eaa78f2bfe7eefac7f3400aa28f6bba0ee3e8f6fa", //RYUI BANG NFT ID
     ],
   },
+  {
+    cid: "bafybeicpyptlluc642qwlyu3xiu5n5jpdpxjk7p4rtjrkm6zwpchzslje4", //01_The_Granite_Crusader_Sword
+    nftId: [
+      "0x9fd3835ffd6a08de4a15d7615effbbc2f5c6d3059acd3242e43229dc21340684", //01_The_Granite_Crusader_Sword NFT ID
+    ],
+  },
+  {
+    cid: "bafybeihhnrvk7ul5gyxgws6guej5buxzivxpidllw2q4qokncosqq7vodq", //02_The_Obsidian_Crusader_Sword
+    nftId: [
+      "0x667ccf9f27ab7b9cdaffed629e11ba9d6e56236b811b551e4de2e9eb42fa5abb", //02_The_Obsidian_Crusader_Sword NFT ID
+    ],
+  },
+  {
+    cid: "bafybeibzghao2k5tduhse2hdv5q4g5cz6gulw3xs55p54vafp5ol4b7xoe", //03_The_Corrupted_Crusader_Sword
+    nftId: [
+      "0xca5f0986dda1077e8a805c0b7f53de6fed44a0b38fdf6e39bc7118d194874e97", //03_The_Corrupted_Crusader_Sword NFT ID
+    ],
+  },
 ];
 
 export { unlockables };
