7a8ff9146da3e080317b83b40e9ac40f4244bccc
Gordiart <sattriel@gmail.com>
2023-03-28 21:34:29 -0400

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -24,6 +24,30 @@ const unlockables: Unlockable[] = [
       "0xca5f0986dda1077e8a805c0b7f53de6fed44a0b38fdf6e39bc7118d194874e97", //03_The_Corrupted_Crusader_Sword NFT ID
     ],
   },
+  {
+    cid: "bafybeidhzspyplucryxbk33ajgarkz46a4qa7h4o2yfa3ze3cr57mppec4", //04_The_Celestial_Carvers
+    nftId: [
+      "0x7c70c5dd8a67209936eb88d6753ae8e2083f94a6f50a46bd61399d0c47a0ae5c", //04_The_Celestial_Carvers NFT ID
+    ],
+  },
+  {
+    cid: "bafybeieu6nvlcqozblyyj3wm2h6optdsbmt5yvm3s24k22ny7fdytzhchu", //05_The_Shadow_Carvers
+    nftId: [
+      "0x30701f843eb1593b32386339aeaa0968eb8a2f8181fe239e318908f88e2646b9", //05_The_Shadow_Carvers NFT ID
+    ],
+  },
+  {
+    cid: "bafybeibqibrkmcb37jkx2wsqhqr3lfjfozemqj5tpx7f3qphdu7hh5emka", //06_Scarlet_Heartseeker_Bow
+    nftId: [
+      "0x695e9e50c17709799e0494898d49b1a436e3afc73b57cf442d762dd9b39e0b06", //06_Scarlet_Heartseeker_Bow NFT ID
+    ],
+  },
+  {
+    cid: "bafybeieshbfgraubkfimtv5yomomyhbx3ay6nylvvvnsjvu3jtjsctrcay", //Purple_Heartseeker_Bow
+    nftId: [
+      "0x82ccbf585502f6c1f9ecee5ea376391c515ea57a6d0612ec797d0e7ae5b51cf8", //Purple_Heartseeker_Bow NFT ID
+    ],
+  },
 ];
 
 export { unlockables };
